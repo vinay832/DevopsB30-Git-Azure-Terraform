49f86294c532d11a65c203aeb6981e7af3edc27a
vinay832 <kompalli.vinay@gmail.com>
2022-12-01 23:04:01 -0600

    added subnets 1,2,3

---
+++ subnets.tf
@@ -0,0 +1,18 @@
+resource "azurerm_subnet" "subnet-1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.devopsb30azrg.name
+  virtual_network_name = azurerm_virtual_network.devopsb30azrg-vnet1.name
+  address_prefixes     = ["10.30.1.0/24"]
+}
+resource "azurerm_subnet" "subnet-2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.devopsb30azrg.name
+  virtual_network_name = azurerm_virtual_network.devopsb30azrg-vnet1.name
+  address_prefixes     = ["10.30.2.0/24"]
+}
+resource "azurerm_subnet" "subnet-3" {
+  name                 = "subnet-3"
+  resource_group_name  = azurerm_resource_group.devopsb30azrg.name
+  virtual_network_name = azurerm_virtual_network.devopsb30azrg-vnet1.name
+  address_prefixes     = ["10.30.3.0/24"]
+}
